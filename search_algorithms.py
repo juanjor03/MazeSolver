ba89bdcce8b1409c01198f71d4e3b4fd1a0d455d
juanjor03 <juarodriguezm@unal.edu.co>
2022-10-02 23:35:07 -0500

    avance Edwin

---
+++ search_algorithms.py
@@ -0,0 +1,60 @@
+
+# Todas las funciones para buscar el camino correcto
+
+def read_csv():
+    pass
+
+def create_txt_search():
+    pass
+
+def create_txt_instructions():
+    pass
+
+def dfs_search():
+    read_csv()
+    """
+    Procedimiento
+    """
+    create_txt_search()
+    create_txt_instructions()
+
+def bfs_search():
+    read_csv()
+    """
+    Procedimiento
+    """
+    create_txt_search()
+    create_txt_instructions()
+
+def iter_deep_search():
+    read_csv()
+    """
+    Procedimiento
+    """
+    create_txt_search()
+    create_txt_instructions()
+
+def uniform_cost_search():
+    read_csv()
+    """
+    Procedimiento
+    """
+    create_txt_search()
+    create_txt_instructions()
+
+def greedy_search():
+    read_csv()
+    """
+    Procedimiento
+    """
+    create_txt_search()
+    create_txt_instructions()
+
+def a_star_search():
+    read_csv()
+    """
+    Procedimiento
+    """
+    create_txt_search()
+    create_txt_instructions()
+
